fe1dbb35565987dac459ce07135f95898f5723fa
hirumi123 <102029504+hirumi123@users.noreply.github.com>
2022-09-28 23:55:01 +0700

    Add files via upload

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+         //return Post::All();
+        $data = array(
+            'id' => "posts",
+            'posts' => Post::all()
+        );
+        return view('template.index', ["title" => "Blog",])->with($data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('template.create', ["title" => "Create a Post",]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $post = new Post;
+        $post->title = $request->input('title');
+        $post->description = $request->input('description');
+        $post->save();
+        return redirect('posts')->with('post_berhasil', 'data telah ditambahkan');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //return Post::find($id);
+        $data = array(
+            'id' => "posts",
+            'posts' => Post::find($id)
+        );
+        return view('template.show', ["title" => "Post",])->with($data);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $data = array(
+            'id' => "posts",
+            'posts' => Post::find($id)
+        );
+        return view('template.edit', ["title" => "Edit Post"])->with($data);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $post = Post::find($id);
+        $post->title = $request->input('title');
+        $post->description = $request->input('description');
+        $post->save();
+        return redirect('posts')->with('post_update', 'data telah diperbarui');
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $post = Post::find($id);
+        $post->delete(); // menghapus data dari database
+        return redirect('posts')->with('post_delete', 'data telah dihapus');
+    }
+}
